a668a972218d24ba872be0720b3befe3bd913615
Alex <amignagaray@gmail.com>
2023-08-03 21:13:02 -0500

    add profileMenu client side

--- next-app-jsm/lib/actions.ts
+++ next-app-jsm/lib/actions.ts
@@ -1,3 +1,4 @@
+import { createUserMutation, getUserQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -10,10 +11,28 @@ const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL
 const client = new GraphQLClient(apiUrl);
 
 const makeGraphQLRequest = async (query: string, 
-   variables = {}) => {
-      try {
-         return await client.request(query, variables);
-      } catch (error) {
-         throw error;
-      }
+variables = {}) => {
+   try {
+      return await client.request(query, variables);
+   } catch (error) {
+      throw error;
    }
+}
+
+export const getUser = (email: string) => {
+   client.setHeader('x-api-key', apiKey);
+   return makeGraphQLRequest(getUserQuery, { email });
+}
+
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+   client.setHeader('x-api-key', apiKey);
+   const variables = {
+      input: {
+         name: name, 
+         email: email, 
+         avatarUrl: avatarUrl
+      }
+   };
+
+   return makeGraphQLRequest(createUserMutation, variables);
+}
